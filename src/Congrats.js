eb18d40302e178ff111258b5943f5893482b3e50
leonardo neves <leonardo.neves@ibm.com>
2021-10-02 13:03:06 -0300

    Congrats component

---
+++ src/Congrats.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+
+/**
+ * Fucntional react component for congratulatory message.
+ * @fucntion
+ * @param {object} props - React props. 
+ * @returns {JSX.Element} - Rendered component ( or null if success prop is false)
+ */
+
+export default (props) => {
+    if (props.success) {
+        return (
+            <div data-test="component-congrats">
+                <span data-test="congrats-message">
+                    Congratulations! You guessed the word!
+                </span>
+            </div>
+        )
+    } else {
+        return (
+            <div data-test="component-congrats"></div>
+        )
+    }
+} 
